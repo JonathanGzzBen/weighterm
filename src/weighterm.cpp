5df1891497557f9ceaa0edeb2c3d4af19efa690a
JonathanGzzBen <jonathangzzben@gmail.com>
2021-11-03 16:26:47 -0600

    Create initial usage help

--- src/weighterm.cpp
+++ src/weighterm.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 
-int main() { std::cout << "Hello" << std::endl; }
+#include "docopt.h"
+
+const char USAGE[] =
+    R"(weighterm. Weight tracking terminal application
+
+    Usage:
+      weighterm register <weight>
+      weighterm (-h | --help)
+      weighterm --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+)";
+
+int main(int argc, char** argv) {
+  std::map<std::string, docopt::value> args =
+      docopt::docopt(USAGE, {argv + 1, argv + argc}, true, "weighterm 0.1");
+  for (auto const& arg : args) {
+    std::cout << arg.first << " " << arg.second << std::endl;
+  }
+  if (args.at("<weight>")) {
+    std::cout << "Registering weight" << std::endl;
+  }
+  return 0;
+}
