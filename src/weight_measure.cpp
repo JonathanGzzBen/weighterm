2234b2f685ee926138386f19acb9e715e2e9b3e7
JonathanGzzBen <jonathangzzben@gmail.com>
2021-11-11 18:15:25 -0600

    Format output of list command

--- src/weight_measure.cpp
+++ src/weight_measure.cpp
@@ -3,6 +3,8 @@
 WeightMeasure::WeightMeasure(int id, double weight)
     : m_id(id), m_weight(weight) {}
 
+int WeightMeasure::get_id() { return m_id; }
+
 double WeightMeasure::get_weight() { return m_weight; }
 
 void WeightMeasure::set_weight(double weight) { m_weight = weight; }
